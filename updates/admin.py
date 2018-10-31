9ff5cc167d617801851753f65d18f23a5ac43e27
yanadhorn <yanadhorn@gmail.com>
2018-10-31 23:37:29 +0700

    delete restapi model

--- updates/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Update as UpdateModel
-
-admin.register(UpdateModel)
--- updates/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UpdatesConfig(AppConfig):
-    name = 'updates'
