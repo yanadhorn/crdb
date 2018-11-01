aa869c13809880348949a0f1075286e14196875f
yanadhorn <yanadhorn@gmail.com>
2018-11-01 11:28:21 +0700

    again

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def dashboard(request):
-    return render(request, 'dashboard/templates/base.html', {'dashboard': dashboard})
+    return render(request, 'dashboard/base.html', {'dashboard': dashboard})
